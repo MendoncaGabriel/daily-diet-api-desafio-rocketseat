ee36490cbb6a83beb152e9ec158fc9de9105831c
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-12-12 13:37:09 -0400

    firs commit!

---
+++ env/index.ts
@@ -0,0 +1,26 @@
+import { config } from 'dotenv'
+import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_URL: z.string()
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables!')
+  throw new Error('Invalid environment variables!')
+}
+
+const env = _env.data
+
+export { env }
