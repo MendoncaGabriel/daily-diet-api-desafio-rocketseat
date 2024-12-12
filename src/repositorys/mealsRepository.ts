f6e6426dfc605def40dc57158c2d11c12e942f2b
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-12-12 15:38:38 -0400

    save

--- src/repositorys/mealsRepository.ts
+++ src/repositorys/mealsRepository.ts
@@ -10,9 +10,48 @@ class Meals {
       is_on_diet: boolean
       date: Date
     }
-  ){
+  ) {
     await knex('meals').insert(data)
   }
+
+  async update(
+    { id, data }: {
+      data: {
+        id: string,
+        user_id: string,
+        name: string,
+        description: string
+        is_on_diet: boolean
+        date: Date
+      },
+      id: string
+    }
+  ) {
+    await knex('meals').where({ id }).update({
+      id, data
+    })
+  }
+
+  async delete(
+    { id }: { id: string }
+  ) {
+    await knex('meals').where({ id }).delete()
+  }
+
+  async listUserMeals(
+    { user_id }: { user_id: string }
+  ) {
+    const meals = await knex('meals').where({ user_id }).select()
+    return meals
+  }
+
+  async listMealById(
+    { id }: { id: string }
+  ) {
+    const meal = await knex('meals').where({ id }).select()
+    return meal
+  }
+
 }
 
 export { Meals }
