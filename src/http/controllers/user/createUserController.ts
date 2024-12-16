ae216b8c1926de77adbe9bf37b6aa4fcb8bc1f18
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-12-16 00:12:14 -0400

    feat: controller create user

--- src/http/controllers/user/createUserController.ts
+++ src/http/controllers/user/createUserController.ts
@@ -1,19 +1,24 @@
 import { z } from "zod";
 import { KnexUserRepository } from "../../../repositorys/knex/knexUserRepository";
 import { CreateUserUseCase } from "../../../usecases/user/createUserUseCase";
+import { FastifyRequest } from "fastify";
 
 class CreateMealController {
-  async handle() {
+  async handle(req: FastifyRequest) {
 
     const createUserSchema = z.object({
-
+      session_id: z.string().optional(),
+      email: z.string(),
+      name: z.string()
     })
+
+    const { email, name, session_id } = createUserSchema.parse(req.body)
 
     const userRepository = new KnexUserRepository()
     const createUserUsecase = new CreateUserUseCase(userRepository)
 
     await createUserUsecase.execute({
-      
+      email, name, session_id
     })
   }
 }
